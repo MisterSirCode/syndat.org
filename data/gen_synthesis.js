a1aa4a35f78867dc9dc1c970640896dff9e38954
Sir Code <taylorschneider07@yahoo.com>
2024-07-09 16:01:24 -0400

    More changes for synthesis generation

--- data/gen_synthesis.js
+++ data/gen_synthesis.js
@@ -1,33 +1,27 @@
 const fs = require('fs');
 const synthesis = require('./json/synthesis.json');
-const revision = synthesis[0].revision;
-let materialTemplate = fs.readFileSync('./synthesisTemplate.html', { encoding: 'utf-8', flag: 'r' });
-
-function completionStatus(mat) {
-    let points = 0;
-    if (Object.keys(mat.chem_prop).length > 0) points++;
-    if (Object.keys(mat.optic_prop).length > 0 || mat.bypass_optic) points++;
-    if (Object.keys(mat.cry_prop).length > 0) points++;
-    if (mat.desc) points++;
-    return points;
-}
+const revision = synthesis.revision;
+let synthesisTemplate = fs.readFileSync('./synthesisTemplate.html', { encoding: 'utf-8', flag: 'r' });
 
 function generateTemplates() {
-    materials.shift(); // Clear extra info sector from the working database
+    let keys = Object.keys(synthesis);
+    // Clear extra info sector from the working database
+    keys.shift();
     let genlist = [];
-    for (let i = 0; i < materials.length; i++) {
+    for (let i = 0; i < keys.length; i++) {
         console.clear();
-        console.log(`Finishing Template ${i + 1} / ${materials.length}`);
-        let mat = materials[i];
-        let template = materialTemplate;
+        console.log(`Finishing Template ${i + 1} / ${keys.length}`);
+
+        let key = keys[i];
+        let method = synthesis[key];
+        let template = synthesisTemplate;
 
         // Create Truncated List
 
-        let status = completionStatus(mat);
         genlist.push([
-            mat.id,
-            mat.label,
-            status
+            key,
+            method.title,
+            method.trunc
         ]);
 
         // Begin Template Construction
@@ -37,48 +31,17 @@ function generateTemplates() {
             template = template.replace(`<div class="pageSectionItem">${tag}</div><div class="pageSectionValue">${repl}</div>`, '');
         }
 
-        // Smaller Replacements
-
-        fix('TITLE', mat.label);
-
-        // Write Article
-
-        if (mat.desc) fix('ARTICLE', mat.desc);
-        else fix('ARTICLE', 'Not much is known about this material besides synthesis');
-
-        // Add Production Methods
-
-        let methods = mat.synthesis;
-
-        let tempList = [];
-
-        for (var m = 0; m < methods.length; m++) {
-            let method = methods[m];
-            let data = synthesis[method];
-            let temp = `
-            <div class="specialtyGridItem">
-                <img class="specialtyGridImage">
-                <div class="specialtyGridContent">
-                <div class="specialtyGridTitle">PRODTITLE</div>
-                <div class="specialtyGridDesc">PRODDESC</div>
-                </div>
-            </div>`;
-            temp = temp.replace('PRODTITLE', data.title);
-            temp = temp.replace('PRODDESC', data.trunc);
-            tempList.push(temp);
-        }
-
-        fix('SYNLIST', tempList.join(""));
+        // Replacements
 
         fix('REV', revision);
 
         console.log('Writing Template...');
-        if (!fs.existsSync(`../public/synthesis/methods/${mat.label}/`))
-            fs.mkdirSync(`../public/synthesis/methods/${mat.label}/`);
-        fs.writeFileSync(`../public/synthesis/methods/${mat.label}/index.html`, template);
+        if (!fs.existsSync(`../public/synthesis/methods/${key}/`))
+            fs.mkdirSync(`../public/synthesis/methods/${key}/`);
+        fs.writeFileSync(`../public/synthesis/methods/${key}/index.html`, template);
     }
     console.log('Finished');
-    //fs.writeFileSync('../public/materials/generatedList.json', JSON.stringify(genlist));
+    fs.writeFileSync('../public/synthesis/generatedList.json', JSON.stringify(genlist));
 }
 
 generateTemplates();
