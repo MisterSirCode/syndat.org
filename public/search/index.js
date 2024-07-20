005471bc53ae9b8a0b1e97fddd29f7c9896e765d
Sir Code <taylorschneider07@yahoo.com>
2024-07-19 23:01:27 -0400

    Add material searching script

--- public/search/index.js
+++ public/search/index.js
@@ -25,9 +25,13 @@ conf.addEventListener('click', async function(el) {
     const rawtext = input.value.toLowerCase();
     if (rawtext.replace(/\s+/g, '').length < 1) return;
     const tokens = rawtext.split(' ');
+
+    const rawTokens = input.value.split(' ');
+
     let matches = [];
     for (var i = 0; i < tokens.length; i++) {
         const token = tokens[i];
+        const rawToken = rawTokens[i];
         for (var s = 0; s < data[0].length; s++) {
             const syn = data[0][s];
             if (syn[1].toLowerCase().includes(token)) matches.push([`syn.${syn[0]}.name`, syn[1]]);
@@ -36,6 +40,13 @@ conf.addEventListener('click', async function(el) {
                 if (filteredAliases.length > 0) matches.push([`syn.${syn[0]}.aliases`, filteredAliases]);
             }
             if (syn[3].toLowerCase().includes(token)) matches.push([`syn.${syn[0]}.disc`, syn[3]]);
+        }
+        for (var m = 0; m < data[1].length; m++) {
+            const mat = data[1][m];
+            if (mat[0].toLowerCase().includes(token)) matches.push([`mat.${mat[0]}.name`, mat[0]]);
+            if (mat[1].toLowerCase().includes(token)) matches.push([`mat.${mat[0]}.aliases`, mat[1]]);
+            if (mat[2].toLowerCase().includes(token)) matches.push([`mat.${mat[0]}.chemical`, mat[2]]);
+            if (mat[3].replace('<sub>', '').replace('</sub>', '').includes(rawToken)) matches.push([`mat.${mat[0]}.formula`, mat[3]]);
         }
     }
     console.log('Matches: ', matches);
