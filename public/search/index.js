0f3976408b94e6000bcd6ae7399484ab745608d0
Sir Code <taylorschneider07@yahoo.com>
2024-07-19 21:58:15 -0400

    Add basic search script

--- public/search/index.js
+++ public/search/index.js
@@ -1,1 +1,10 @@
 const conf = document.querySelector('.pageSearchConfirm');
+const input = document.querySelector('.pageSearchInput');
+
+conf.addEventListener('click', (el) => {
+    const rawtext = input.value.toLowerCase();
+    const tokens = rawtext.split(' ');
+    for (var i = 0; i < tokens.length; i++) {
+        
+    }
+});
