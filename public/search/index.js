6338762bd08194fa04de5642f57fb64090aa2da2
Sir Code <taylorschneider07@yahoo.com>
2024-07-19 22:08:52 -0400

    Update search page a bit

--- public/search/index.js
+++ public/search/index.js
@@ -1,10 +1,19 @@
 const conf = document.querySelector('.pageSearchConfirm');
 const input = document.querySelector('.pageSearchInput');
+const loading = true;
 
-conf.addEventListener('click', (el) => {
+async function load() {
+    const res = await fetch("summary.json").catch((err) => { console.error(`Info Failed to Load with Error: ${err}`); });
+    const info = await res.json();
+}
+
+load();
+
+conf.addEventListener('click', async function(el) {
+    if (loading) return;
     const rawtext = input.value.toLowerCase();
     const tokens = rawtext.split(' ');
     for (var i = 0; i < tokens.length; i++) {
-        
+
     }
 });
