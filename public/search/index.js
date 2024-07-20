83f0d23297089f5965cc58b6c6a3e7e5c5dacb35
Sir Code <taylorschneider07@yahoo.com>
2024-07-19 22:48:15 -0400

    Add basic working search script for synthesis info

--- public/search/index.js
+++ public/search/index.js
@@ -1,5 +1,6 @@
 const conf = document.querySelector('.pageSearchConfirm');
 const input = document.querySelector('.pageSearchInput');
+let data;
 let loading = true;
 
 async function load() {
@@ -11,6 +12,8 @@ async function load() {
         if (info.length > 0) {
             loading = false;
             input.setAttribute('placeholder', 'Search...');
+            data = info;
+            console.log(data);
         }
     } else input.setAttribute('placeholder', 'Search Load Failed. Contact Dev');
 }
@@ -20,8 +23,20 @@ load();
 conf.addEventListener('click', async function(el) {
     if (loading) return;
     const rawtext = input.value.toLowerCase();
+    if (rawtext.replace(/\s+/g, '').length < 1) return;
     const tokens = rawtext.split(' ');
+    let matches = [];
     for (var i = 0; i < tokens.length; i++) {
-
+        const token = tokens[i];
+        for (var s = 0; s < data[0].length; s++) {
+            const syn = data[0][s];
+            if (syn[1].toLowerCase().includes(token)) matches.push([`syn.${syn[0]}.name`, syn[1]]);
+            if (syn[2].length > 0) {
+                let filteredAliases = syn[2].filter(str => str.toLowerCase().includes(token));
+                if (filteredAliases.length > 0) matches.push([`syn.${syn[0]}.aliases`, filteredAliases]);
+            }
+            if (syn[3].toLowerCase().includes(token)) matches.push([`syn.${syn[0]}.disc`, syn[3]]);
+        }
     }
+    console.log('Matches: ', matches);
 });
