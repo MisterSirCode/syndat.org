861d23cf0d970845521b22dd0bbf150a6a8072c6
Sir Code <taylorschneider07@yahoo.com>
2024-08-05 12:10:56 -0400

    Allow parody change to work with generator

--- data/gen_synthesis.js
+++ data/gen_synthesis.js
@@ -53,7 +53,7 @@ function generateTemplates() {
         fix('SYNREF', 'Information about ' + method.prefix + method.title);
         fix('TITLE', method.title);
         fix('SUBTITLE', 'Developed by ' + method.disc);
-        if (method.aliases.length > 0) fix('ALIASES', `<span>Otherwise known by: ${method.aliases.join(', ')}</span><br><br>`);
+        if (method.aliases.length > 0) fix('ALIASES', `<span>Otherwise known by ${method.aliases}</span><br><br>`);
         else fix('ALIASES', '');
         fix('ARTICLE', method.desc.replace('<br>', '<br><br>'));
         if (method.history) fix('HISTORY', method.history.replace('<br>', '<br><br>'));
